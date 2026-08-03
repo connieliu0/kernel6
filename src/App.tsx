c3a57b332836c788017cc19ac992d699157db8fa
Connie Liu <cliu55567@gmail.com>
2026-08-03 13:23:14 -0400

    analytics

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,13 @@
+import { Analytics } from "@vercel/analytics/react";
 import { ConsumptionScene } from "./components/ConsumptionScene";
 
 function App() {
-  return <ConsumptionScene />;
+  return (
+    <>
+      <ConsumptionScene />
+      <Analytics />
+    </>
+  );
 }
 
 export default App;
